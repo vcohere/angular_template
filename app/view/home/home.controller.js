73ba37f82c7691f691cc1b3a86e54d778a53c188
U-GOP-LINK\n.savry <n.savry@LGOP227.gop.link>
2016-05-04 19:26:07 +0200

    add loading

--- app/view/home/home.controller.js
+++ app/view/home/home.controller.js
@@ -1,2 +1,14 @@
 mainModule.controller('homeCtrl', function($scope, homeRest) {
+	var self = this;
+
+	self.getData = function() {
+		self.load = true;
+		self.load = false;
+	}
+
+	self.doRefresh = function() {
+		self.getData();
+	}
+
+	self.getData();
 });
