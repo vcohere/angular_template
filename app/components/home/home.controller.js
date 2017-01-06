c24c21730c98fce7a49e3746fa1fc08f88d93b09
Valentin Cohere <vcohere@student.42.fr>
2017-01-06 12:30:08 +0100

    Update home.controller.js

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,11 +1,3 @@
 mainModule.controller('homeCtrl', function($scope, homeRest) {
-	var self = this;
-
-	self.getData = function() {
-		self.load = true;
-		// Only business logic in the controller part
-		self.load = false;
-	}
-
-	self.getData();
+	// Only business logic in the controller part
 });
